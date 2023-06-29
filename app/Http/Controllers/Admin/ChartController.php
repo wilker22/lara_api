983ca4d96fecccbfd0f59de18b1fab4b8249403d
Wilker <wilker22@outlook.com>
2023-06-29 09:47:55 -0300

    endpoint http://127.0.0.1:8000/api/chartdata

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chart;
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
 {
-    //
+    public function onAllSelect()
+    {
+        $result = Chart::all();
+         return $result;
+    }
 }
