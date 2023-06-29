3b2d78d005cda80bfc6203eed2ed670d5e62d1e0
Wilker <wilker22@outlook.com>
2023-06-29 10:09:11 -0300

    endpoint para envio de contato/mensagem

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function onContactSend(Request $request)
+    {
+        $result = Contact::insert([
+            'name' => $request->name,
+            'email' => $request->email,
+            'message' => $request->message
+        ]);
+
+        if($result == true){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ChartController;
 use App\Http\Controllers\Admin\ClientReviewController;
+use App\Http\Controllers\Admin\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/chartdata', [ChartController::class, 'onAllSelect']);
 Route::get('/clientreview', [ClientReviewController::class, 'onAllSelect']);
+Route::post('/contactsend', [ContactController::class, 'onContactSend']);
