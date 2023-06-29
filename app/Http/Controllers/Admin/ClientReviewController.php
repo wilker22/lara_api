e590114c6f6a656fa1b27ad01feb69c3557d5c9b
Wilker <wilker22@outlook.com>
2023-06-29 09:58:28 -0300

    clientreview endpoint

--- app/Http/Controllers/Admin/ClientReviewController.php
+++ app/Http/Controllers/Admin/ClientReviewController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ClientReview;
 use Illuminate\Http\Request;
 
 class ClientReviewController extends Controller
 {
-    //
+    public function onAllSelect()
+    {
+        $result = ClientReview::all();
+        return $result;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ChartController;
+use App\Http\Controllers\Admin\ClientReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/chartdata', [ChartController::class, 'onAllSelect']);
+Route::get('/clientreview', [ClientReviewController::class, 'onAllSelect']);
